ce2851fdaaa7214e7dc3f1c3d279402e87d384a5
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-02-06 15:00:55 +0100

    src: remove comments from wit.rs
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/wit.rs
+++ src/wit.rs
@@ -1,11 +1,5 @@
 wit_bindgen::generate!({
-    // the name of the world in the `*.wit` input file
     world: "llm",
-
-    // For all exported worlds, interfaces, and resources, this specifies what
-    // type they're corresponding to in this module. In this case the `MyHost`
-    // struct defined below is going to define the exports of the `world`,
-    // namely the `run` function.
     exports: {
         world: Exports,
     },
