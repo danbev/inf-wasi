e593a1736fa38d8c74a9608822bf95f94716eaea
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-03-12 13:23:42 +0100

    src: update the help message for the CLI
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- bindings/rust/src/main.rs
+++ bindings/rust/src/main.rs
@@ -3,12 +3,12 @@ use rust_bindings::WasmtimeInference;
 use std::path::PathBuf;
 
 fn main() -> wasmtime::Result<()> {
-    let matches = App::new("Inference CLI")
+    let matches = App::new("Wasmtime llama.cpp inference")
         .arg(
             Arg::with_name("component_path")
                 .long("component-path")
                 .value_name("COMPONENT_PATH")
-                .help("Sets the component wasm file path")
+                .help("Path to the component wasm")
                 .takes_value(true)
                 .required(true),
         )
@@ -16,7 +16,7 @@ fn main() -> wasmtime::Result<()> {
             Arg::with_name("model_dir")
                 .long("model-dir")
                 .value_name("MODEL_DIR")
-                .help("Sets the model directory path")
+                .help("The model directory to preopen and make available to the wasm component")
                 .takes_value(true)
                 .required(true),
         )
@@ -24,8 +24,10 @@ fn main() -> wasmtime::Result<()> {
 
     let component_path = PathBuf::from(matches.value_of("component_path").unwrap());
     let model_dir = PathBuf::from(matches.value_of("model_dir").unwrap());
+
     let inference = WasmtimeInference::new(component_path, model_dir)?;
     let result = inference.compute(None);
+
     println!("Result: {}", result);
     Ok(())
 }
